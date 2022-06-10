cb59f71d0f56b6b7109e5a373510e8871eda2d06
Shan Gao <84673611+sgao52@users.noreply.github.com>
2022-06-10 11:22:54 -0700

    Update WOWJump.py

--- WOWJump.py
+++ WOWJump.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 
 # Retrieved from Bilibili
+# Have to activate your WOW main interface to keep your character jumping per 30 seconds
 for num in range (0, 1000):
     print('Prepare to Jump')
     pyautogui.keyDown('space')
